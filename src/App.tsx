40367b64dbb240e45ffd4a2ac033e2bbd213da49
grittyDV <164671274+grittyDV@users.noreply.github.com>
2024-12-08 23:50:42 +0100

    Fix module resolution and TypeScript setup

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Footer from './components/layout/Footer';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
