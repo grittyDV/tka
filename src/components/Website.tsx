1b229ca8184d9152ed6af08bf2aaf22b52229b48
grittyDV <164671274+grittyDV@users.noreply.github.com>
2024-12-27 16:49:10 +0200

    Update Website component to include About and FAQ sections

--- src/components/Website.tsx
+++ src/components/Website.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { useTranslations } from '../contexts/TranslationContext';
 import { Header } from './Header';
 import { Hero } from './Hero';
+import { About } from './About';
 import { Services } from './Services';
+import { FAQ } from './FAQ';
 import { Contact } from './Contact';
 import { Footer } from './Footer';
 
@@ -11,7 +13,9 @@ export const Website = () => {
     <div className="min-h-screen bg-gray-50">
       <Header />
       <Hero />
+      <About />
       <Services />
+      <FAQ />
       <Contact />
       <Footer />
     </div>
